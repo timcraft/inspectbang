3d1995912e0d54781e86f8fe1f0853ec44f07d8e
Tim Craft <mail@timcraft.com>
2012-10-29 16:53:51 +0000

    Make it work with sinatra

--- lib/inspectbang.rb
+++ lib/inspectbang.rb
@@ -1,21 +1,31 @@
 require 'cgi'
 
 module InspectBang
-  class Exception < StandardError; end
+  if defined?(Rails)
+    class Exception < StandardError; end
 
-  module Method
-    def inspect!
-      raise Exception.new(self.inspect)
-    end
-  end
-
-  class Railtie < Rails::Railtie
-    initializer 'inspectbang' do |app|
-      ActionController::Base.rescue_from(Exception) do |exception|
-        render :text => CGI.escapeHTML(exception.message)
+    module Method
+      def inspect!
+        raise Exception.new(self.inspect)
       end
-
-      Object.send(:include, Method)
     end
+
+    class Railtie < Rails::Railtie
+      initializer 'inspectbang' do |app|
+        ActionController::Base.rescue_from(Exception) do |exception|
+          render :text => CGI.escapeHTML(exception.message)
+        end
+
+        Object.send(:include, Method)
+      end
+    end
+  elsif defined?(Sinatra)
+    module Method
+      def inspect!
+        throw :halt, [200, CGI.escapeHTML(self.inspect)]
+      end
+    end
+
+    Object.send(:include, Method)
   end
 end
